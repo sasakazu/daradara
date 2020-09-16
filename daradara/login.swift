914f031072326d8b25e1bf4619a566b4bde8f27b
笹倉一也 <kazuya.sasakura@gmail.com>
2020-09-16 20:31:46 +0900

    ログインとサインアップテキストフィールド設置

---
+++ daradara/login.swift
@@ -0,0 +1,22 @@
+//
+//  login.swift
+//  daradara
+//
+//  Created by 笹倉一也 on 2020/09/16.
+//  Copyright © 2020 笹倉一也. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ daradara/signup.swift
@@ -0,0 +1,23 @@
+//
+//  signup.swift
+//  daradara
+//
+//  Created by 笹倉一也 on 2020/09/16.
+//  Copyright © 2020 笹倉一也. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class signup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
